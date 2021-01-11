80fb58df9f418cf6a308861fa5b04484cc8352fb
Shashaank Singh <tapash2004@outlook.com>
2021-01-11 12:07:35 +0530

    Update code.py

--- code.py
+++ code.py
@@ -100,9 +100,9 @@ def test():
             if request.json["data"]["options"][0]["value"] == 'Cat':
                 if len(request.json["data"]["options"]) == 1:
                     res = requests.get('https://api.thecatapi.com/v1/images/search?size=small&mime_types=jpg,png')
-                elif request.json["data"]["options"][1]["value"] == "False":
+                elif request.json["data"]["options"][0]["value"] == "False":
                     res = requests.get('https://api.thecatapi.com/v1/images/search?size=small&mime_types=jpg,png')
-                else request.json["data"]["options"][1]["value"] == "True":
+                else request.json["data"]["options"][0]["value"] == "True":
                     res = requests.get('https://api.thecatapi.com/v1/images/search?size=small&mime_types=gif')
                 imgjson = res.json()
                 imgurl = imgjson[0]['url']
