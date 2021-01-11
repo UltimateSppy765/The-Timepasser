168b5dee8a7dff79d7c847503f7c90cbb75e64b2
Shashaank Singh <tapash2004@outlook.com>
2021-01-11 12:21:01 +0530

    Update code.py

--- code.py
+++ code.py
@@ -98,9 +98,9 @@ def test():
             })
         elif cmd_name == "anipic":
             if request.json["data"]["options"][0]["value"] == 'Cat':
-                if len(request.json["data"]["options"]) == 1 or request.json["data"]["options"][1]["value"] == "False":
+                if len(request.json["data"]["options"]) == 1 or request.json["data"]["options"][1]["value"] == false:
                     res = requests.get('https://api.thecatapi.com/v1/images/search?size=small&mime_types=jpg,png')
-                elif request.json["data"]["options"][1]["value"] == "True":
+                elif request.json["data"]["options"][1]["value"] == true:
                     res = requests.get('https://api.thecatapi.com/v1/images/search?size=small&mime_types=gif')
                 imgjson = res.json()
                 imgurl = imgjson[0]['url']
