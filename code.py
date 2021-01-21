495bf8b8190b6b625d010d7c2453af848574deb4
Shashaank Singh <tapash2004@outlook.com>
2021-01-21 12:21:07 +0530

    Hid the Client ID through Config Vars.

--- code.py
+++ code.py
@@ -7,7 +7,7 @@ from flask import Flask, request, jsonify,abort
 from discord_interactions import verify_key_decorator, InteractionType, InteractionResponseType
 
 
-CLIENT_PUBLIC_KEY = '731d23ca5906b7f156c4f606ad29ae1d6efaf82661889227e9226f8694d5fbce'
+CLIENT_PUBLIC_KEY = os.environ['CLIENT_ID']
 
 app = Flask(__name__)
 
