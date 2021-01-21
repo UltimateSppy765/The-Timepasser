a413e8b2faa8ddaa55f3f7ed7f71e946b49c1937
Shashaank Singh <tapash2004@outlook.com>
2021-01-21 12:48:17 +0530

    Update code.py

--- code.py
+++ code.py
@@ -57,7 +57,10 @@ def code():
                 elif cmd_name == "dice":
                     dice = [1,2,3,4,5,6,"**The dice got stuck against the wall. Try Again!**","**The dice got lost. Try Again!**"]
                     roll = choice(dice)
-                    content = f"The dice rolled {roll}. <:dice_1:755891608859443290>" if type(roll) == int else roll
+                    if type(roll) == int:
+                        rolle = roll - 1
+                    emlist = ["<:dice_1:755891608859443290>", "<:dice_2:755891608741740635>", "<:dice_3:755891608251138158>", "<:dice_4:755891607882039327>", "<:dice_5:755891608091885627>", "<:dice_6:755891607680843838>"]
+                    content = f"The dice rolled {roll}. {emlist[rolle]}" if type(roll) == int else roll
                     return jsonify(
                         {
                             "type": 4,
