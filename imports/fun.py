4fc5b92a42d809c2202d195f9c56ee8c853aeb11
Shashaank Singh <tapash2004@outlook.com>
2021-05-01 10:16:41 +0530

    Fixed some stuff, added some stuff

--- imports/fun.py
+++ imports/fun.py
@@ -3,7 +3,7 @@ from random import choice
 
 baseurl=os.environ['BASE_URL']
 
-def dice(iid:str,token:str):
+def dice(aid:str,iid:str,token:str):
   dice = [1,2,3,4,5,6,"**The dice got stuck against the wall. Try Again!** :exploding_head:","**The dice got lost. Try Again!** :thinking:"]
   roll = choice(dice)
   emojis = ["<:dice_1:755891608859443290>", "<:dice_2:755891608741740635>", "<:dice_3:755891608251138158>", "<:dice_4:755891607882039327>", "<:dice_5:755891608091885627>", "<:dice_6:755891607680843838>"]
@@ -17,4 +17,5 @@ def dice(iid:str,token:str):
   dicerolled = {
         "content": f"The dice rolled {roll} {emojis[roll-1]}" if type(roll) == int else roll
   }
-  requests.patch(f"{baseurl}interactions/{iid}/{token}/callback",json=dicerolled)
+  await asyncio.sleep(1)
+  requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original"",json=dicerolled)
