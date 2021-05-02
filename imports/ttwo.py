fb6d46dbb76daf644868b6e906635794f555ead9
Shashaank Singh <tapash2004@outlook.com>
2021-05-03 01:27:50 +0530

    Brought Simon to life!

--- imports/ttwo.py
+++ imports/ttwo.py
@@ -14,6 +14,8 @@ def slashc(r):
                 return misc.aboutme(subc=r.json["data"]["options"][0]["name"],uid=r.json["member"]["user"]["id"])
             elif cmdname=="dice":
                 return fun.dice(aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+            elif cmdname=="simon":
+                return fun.simon(text=r.json["data"]["options"][0]["value"])
             elif cmdname=="echo":
                 return fun.echo(text=r.json["data"]["options"][0]["value"],uname=r.json["member"]["user"]["username"],id=r.json["member"]["user"]["id"],disc=r.json["member"]["user"]["discriminator"],av=r.json["member"]["user"]["avatar"])
             else:
