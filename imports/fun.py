56813682c36c44fb2e65a7e6074d83d911def9f8
Shashaank Singh <tapash2004@outlook.com>
2021-05-05 02:14:11 +0530

    Avatar is here!

--- imports/fun.py
+++ imports/fun.py
@@ -73,3 +73,27 @@ def guessnum(guess:int,aid:str,iid:str,token:str):
     sleep(1)
     requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=jsr)
     return
+def avatar(us:str,uname:str,id:str,disc:str,av:str):
+    if us is None:
+        usname=uname
+        avurl=notcmd.usav(id=id,discid=disc,av=av)
+    else:
+        res=requests.get(f"{baseurl}users/{us}",headers={"Authorization":f"Bot {os.environ['BOT_TOKEN']}"})
+        av1=res.json["avatar"]
+        disc1=res.json["discriminator"]
+        usname=res.json["username"]
+        avurl=notcmd.usav(id=us,discid=disc1,av=av1)
+    return {
+        "type": 4,
+        "data": {
+            "embeds": [
+                {
+                    "title": f":mirror: {usname}'s Avatar:",
+                    "color": 3092791
+                    "image": {
+                        "url": avurl
+                    }
+                }
+            ]
+        }
+    }
