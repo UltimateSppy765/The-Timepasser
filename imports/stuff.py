58f25d4b16296b8ce05b9656200030fa1b4be7f1
Shashaank Singh <tapash2004@outlook.com>
2021-05-05 11:09:24 +0530

    Fox non animated

--- imports/stuff.py
+++ imports/stuff.py
@@ -1,2 +1,30 @@
+import requests
+from imports import misc
 def anipic(anim:bool,animal:str):
-    return
+    if animal=="fox":
+        if anim==False
+            foxy=requests.get("https://randomfox.ca/floof").json()
+            url=foxy["image"]
+            cont=":fox: Here's a fox pic for you."
+            ftext="Powered by RandomFox"
+            ficon="https://cdn.discordapp.com/attachments/789798190353743874/798118721091928074/logo.png"
+            greet="Ring-ding-ding-ding-dingeringeding!"
+        else:
+            return misc.existnt(cname="/anipic")
+    elif animal=="cat":
+        return misc.existnt(cname="/anipic")
+    elif animal=="dog":
+        return misc.existnt(cname="/anipic")
+    return {
+        "type": 4,
+        "data": {
+            "content": cont,
+            "embeds": [
+                "title": greet,
+                "color": 3092791,
+                "image": {
+                    "url": url
+                }
+            ]
+         }
+    }
