94a72c0f90e45d63e6f957d51907839a8d4094d3
Shashaank Singh <tapash2004@outlook.com>
2021-05-06 00:42:20 +0530

    42

--- imports/fun.py
+++ imports/fun.py
@@ -62,7 +62,7 @@ def guessnum(guess:int,aid:str,iid:str,token:str):
       "type": 4,
       "data": {
         "flags": 64,
-        "content": "> **You chose 42, and…**\n> <:tickYes:315009125694177281> Bingo! It's correct, I don't need to think of this number for your guess to be correct… \n> *It's always correct!*\nDidn't understand? [__Read this!__](https://en.m.wikipedia.org/wiki/42_(number))"
+        "content": "> **You chose 42, and…**\n> <:tickYes:315009125694177281> Bingo! It's correct, I don't need to think of this number for your guess to be correct… \n> *It's always correct!*\nDidn't understand? [Read this!](https://en.m.wikipedia.org/wiki/Phrases_from_The_Hitchhiker%27s_Guide_to_the_Galaxy)"
       }
     }
   if guess not in range(1,11):
