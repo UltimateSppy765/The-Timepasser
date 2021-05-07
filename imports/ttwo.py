73b4843f162724f8f0c3fe86d057e91969ae3599
Shashaank Singh <tapash2004@outlook.com>
2021-05-07 23:36:52 +0530

    Opened commands to all

--- imports/ttwo.py
+++ imports/ttwo.py
@@ -8,8 +8,6 @@ def slashc(r):
     else:
         try:
             cmdname=r.json["data"]["name"]
-            if r.json["member"]["user"]["id"] not in ["719906610151030795","482526553616416782","479195061792407562","730361955226746923","698200925311074485","770542184310243342"]:
-                return misc.existnt(cmdname)
             if cmdname=="eval":
                 return misc.eval(uid=r.json["member"]["user"]["id"],token=r.json["token"],iid=r.json["id"],sc=r.json["data"]["options"][0]["name"],aid=r.json["application_id"],jsn=r.json["data"]["options"][0]["options"][0]["value"])
             elif cmdname=="aboutme":
