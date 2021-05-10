025aeb205aa86fd131f646a130b05a26f197f613
Shashaank Singh <tapash2004@outlook.com>
2021-05-11 02:52:03 +0530

    Create quote.py

---
+++ imports/commands/quote.py
@@ -0,0 +1,46 @@
+import os,wikiquote,requests
+from imports.utils import qfinder,perspective
+
+baseurl=os.environ['BASE_URL']
+
+def quote(subc:str,query:str,token:str,aid:str,iid:str):
+    if subc=="get":
+        if query=="qotd":
+            titl="Quote of the Day:"
+            ftext="Quotes from Wikiquote"
+            ficon="https://cdn.discordapp.com/attachments/789798190353743874/794948919594450944/QqJDyLtUbgAAAAASUVORK5CYII.png"
+            (qt,autor)=wikiquote.quote_of_the_day()
+        elif query=="random":
+            titl="Random Quote:"
+            ftext="Powered by Quotable"
+            ficon="https://cdn.discordapp.com/attachments/839610105858752522/839610124271484938/download.jpeg"
+            res=requests.get("http://api.quotable.io/random").json()
+            qt=res["content"]
+            autor=res["author"]
+        return {
+            "type": 4,
+            "data": {
+                "embeds": [
+                    {
+                        "color": 3092791,
+                        "title": titl,
+                        "description": f"{qt}\n- {autor}",
+                        "thumbnail": {
+                            "url": "https://cdn.discordapp.com/attachments/789798190353743874/796948926590615572/oie_transparent_1.png"
+                        },
+                        "footer": {
+                            "text": ftext,
+                            "icon_url": ficon
+                        }
+                    }
+                ]
+            }
+        }
+    else:
+        a=perspective.analyse(cont=query)
+        if a is not None:
+            return a
+        requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":5})
+        jsr=notcmd.qsearch(query=query)
+        requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=jsr)
+        return
