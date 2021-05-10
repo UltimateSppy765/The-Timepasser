b9f954c99667bb29ed134a36789ca7665611c11f
Shashaank Singh <tapash2004@outlook.com>
2021-05-11 02:05:04 +0530

    Update ttwo.py

--- imports/interactions/ttwo.py
+++ imports/interactions/ttwo.py
@@ -1,4 +1,5 @@
-from imports import misc,fun, stuff
+from imports.commands import simon,echo,avatar,guessnum,dice
+from imports import misc,stuff
 import traceback
 def slashc(r):
     try:
@@ -13,19 +14,19 @@ def slashc(r):
             elif cmdname=="aboutme":
                 return misc.aboutme(token=r.json["token"],iid=r.json["id"],aid=r.json["application_id"],subc=r.json["data"]["options"][0]["name"],uid=r.json["member"]["user"]["id"])
             elif cmdname=="dice":
-                return fun.dice(aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+                return dice.dice(aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
             elif cmdname=="simon":
-                return fun.simon(r.json["data"]["options"][0]["value"])
+                return simon.simon(r.json["data"]["options"][0]["value"])
             elif cmdname=="echo":
-                return fun.echo(text=r.json["data"]["options"][0]["value"],uname=r.json["member"]["user"]["username"],id=r.json["member"]["user"]["id"],disc=r.json["member"]["user"]["discriminator"],av=r.json["member"]["user"]["avatar"])
+                return echo.echo(text=r.json["data"]["options"][0]["value"],uname=r.json["member"]["user"]["username"],id=r.json["member"]["user"]["id"],disc=r.json["member"]["user"]["discriminator"],av=r.json["member"]["user"]["avatar"])
             elif cmdname=="guessnum":
-                return fun.guessnum(guess=r.json["data"]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"])
+                return guessnum.guessnum(guess=r.json["data"]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"])
             elif cmdname=="avatar":
                 try:
                     use=r.json["data"]["options"][0]["value"]
                 except KeyError:
                     use=None
-                return fun.avatar(us=use,uname=r.json["member"]["user"]["username"],id=r.json["member"]["user"]["id"],disc=r.json["member"]["user"]["discriminator"],av=r.json["member"]["user"]["avatar"])
+                return avatar.avatar(us=use,uname=r.json["member"]["user"]["username"],id=r.json["member"]["user"]["id"],disc=r.json["member"]["user"]["discriminator"],av=r.json["member"]["user"]["avatar"])
             elif cmdname=="quote":
                 return stuff.quote(subc=r.json["data"]["options"][0]["name"],query=r.json["data"]["options"][0]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"])
             elif cmdname=="anipic":
