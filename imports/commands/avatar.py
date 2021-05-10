00f8aca1713a286ad545f1af5986ab128312c3db
Shashaank Singh <tapash2004@outlook.com>
2021-05-11 02:13:50 +0530

    Update avatar.py

--- imports/commands/avatar.py
+++ imports/commands/avatar.py
@@ -1,19 +1,19 @@
-from imports.utils import usav
+from imports.utils import av
 import requests,os
 
 baseurl=os.environ['BASE_URL']
 
-def avatar(us,uname:str,id:str,disc:str,av:str):
+def cmd(us,uname:str,id:str,disc:str,av:str):
     if us is None:
         usname=uname
-        avurl=notcmd.usav(id=id,discid=disc,av=av)
+        avurl=av.usav(id=id,discid=disc,av=av)
     else:
         res=requests.get(f"{baseurl}users/{us}",headers={"Authorization":f"Bot {os.environ['BOT_TOKEN']}"})
         smth=res.json()
         av1=smth["avatar"]
         disc1=smth["discriminator"]
         usname=smth["username"]
-        avurl=notcmd.usav(id=us,discid=disc1,av=av1)
+        avurl=av.usav(id=us,discid=disc1,av=av1)
     return {
         "type": 4,
         "data": {
