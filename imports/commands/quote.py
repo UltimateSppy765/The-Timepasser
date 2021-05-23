2d71e88fd893136a0fb5699fdb2305381e80e8fd
Shashaank Singh <tapash2004@outlook.com>
2021-05-24 03:56:40 +0530

    Update quote.py

--- imports/commands/quote.py
+++ imports/commands/quote.py
@@ -1,44 +1,12 @@
 import os,wikiquote,requests,json
-from imports.utils.quotes import qfinder
+from imports.utils.quotes import qfinder,qget
 from imports.utils import perspective
 
 baseurl=os.environ['BASE_URL']
 
 def cmd(subc:str,query:str,token:str,aid:str,iid:str,usid:str):
     if subc=="get":
-        if query=="qotd":
-            titl="Quote of the Day:"
-            ftext="Quotes from Wikiquote"
-            ficon="https://cdn.discordapp.com/attachments/789798190353743874/794948919594450944/QqJDyLtUbgAAAAASUVORK5CYII.png"
-            (qt,autor)=wikiquote.quote_of_the_day()
-        elif query=="random":
-            titl="Random Quote:"
-            ftext="Powered by Quotable"
-            ficon="https://cdn.discordapp.com/attachments/839610105858752522/839610124271484938/download.jpeg"
-            res=requests.get("http://api.quotable.io/random").json()
-            qt=res["content"]
-            autor=res["author"]
-        return {
-            "type": 4,
-            "data": {
-                "flags": 64,
-                "embeds": [
-                    {
-                        "color": 3092791,
-                        "title": titl,
-                        "description": f"{qt}\n- {autor}",
-                        "thumbnail": {
-                            "url": "https://cdn.discordapp.com/attachments/789798190353743874/796948926590615572/oie_transparent_1.png"
-                        },
-                        "footer": {
-                            "text": ftext,
-                            "icon_url": ficon
-                        }
-                    }
-                ]
-            },
-            "components": [] if query=="qotd" else [{"type":1,"components":[{"type":2,"style":1,"label":"Another One!","custom_id":json.dumps({"bfn":"quote","subc":"getran","userid":usid})}]}]
-        }
+        return qget.getquote(type=query,usid=usid)
     else:
         a=perspective.analyse(cont=query)
         if a is not None:
