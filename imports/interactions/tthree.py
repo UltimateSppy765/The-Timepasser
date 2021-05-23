7d4a5de5643b165a264d6403612b4d0268ef36a9
Shashaank Singh <tapash2004@outlook.com>
2021-05-24 00:03:50 +0530

    Update tthree.py

--- imports/interactions/tthree.py
+++ imports/interactions/tthree.py
@@ -1,6 +1,11 @@
-import traceback
+import traceback,json
 from imports.buttons import *
 from imports.utils import fail
 
 def tthree(r):
+    try:
+        b=json.loads(r.json["data"]["custom_id"])
+        bname=b["bfn"]
+        if bname=="dicereroll":
+            return dicereroll.btn(binfo=b,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
     return
