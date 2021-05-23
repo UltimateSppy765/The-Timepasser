e2e3ca92d646b7bec0d5d106c85f0df5b223d86e
Shashaank Singh <tapash2004@outlook.com>
2021-05-24 00:19:16 +0530

    Update dicereroll.py

--- imports/buttons/dicereroll.py
+++ imports/buttons/dicereroll.py
@@ -1,7 +1,37 @@
 import os,json,requests
 from time import sleep
-
+from imports.utils.common import diceroll
 baseurl=os.environ['BASE_URL']
 
 def btn(usid:str,iid:str,aid:str,token:str,binfo):
+    if usid!=binfo["userid"]:
+        return {
+            "type": 4,
+            "data": {
+                "flags": 64,
+                "content": "<:tickNo:315009174163685377> You can't reroll someone else's dice can you?"
+            }
+        }
+    rolling={
+        "type": 7,
+        "data": {
+            "flags": 64,
+            "content": "<a:loading:747680523459231834> Rerolling the Dice…",
+            "components": [
+                {
+                    "type": 1,
+                    "components": [
+                        {
+                            "type": 2,
+                            "style": 2,
+                            "custom_id": json.dumps(binfo),
+                            "disabled": True,
+                            "label": "Rerolling..."
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+    requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json=rolling)
     return
