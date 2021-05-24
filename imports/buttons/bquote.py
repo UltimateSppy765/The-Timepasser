0843937a9b51e1bf6de5ea484b59be50642599ec
Shashaank Singh <tapash2004@outlook.com>
2021-05-25 03:17:40 +0530

    Update bquote.py

--- imports/buttons/bquote.py
+++ imports/buttons/bquote.py
@@ -14,14 +14,15 @@ def btn(aid:str,iid:str,token:str,binfo,usid:str):
         }
     if binfo["subc"]=="getran":
         requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"components":[]}})
-        requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=qget.getquote(type="bran",userid=binfo["userid"]))
+        requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json=qget.getquote(type="bran",userid=binfo["userid"]))
         return
     else:
-        requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":6})
+        requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"components":[]}})
         a=qfinder.qres(query=binfo["query"])
         if binfo["subc"]=="passre":
-            requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json={"components":[]})
-            requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json=a)
+            c=requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json={"content":"<a:typing:597589448607399949> Searching for Quotes..."})
+            print(c.json())
+            requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/{c.json()["message"]["id"]}",headers={"Content-Type": "application/json"},json=a)
         else:
             requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=a)
         return
