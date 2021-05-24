45ac8b4b88c932f35061a6f8390ddcf3a0338853
Shashaank Singh <tapash2004@outlook.com>
2021-05-24 18:25:51 +0530

    I can guarantee some error will happen

--- imports/buttons/bquote.py
+++ imports/buttons/bquote.py
@@ -1,4 +1,24 @@
 import json,os,requests
+from imports.utils.quotes import qget,qfinder
 
 def btn(aid:str,iid:str,token:str,binfo,usid:str):
-    return
+    if usid!=binfo["userid"]:
+        return {
+            "type": 4,
+            "data": {
+                "flags": 64,
+                "content": "<:tickNo:315009174163685377> You can't use that button on someone else's message, please try on your own one."
+            }
+        }
+    if binfo["subc"]=="getran":
+        requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"components":[]}})
+        requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json=qget.getquote(type="random",userid=binfo["userid"]))
+        return
+    else:
+        requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":6})
+        a=qfinder.qres(query=binfo["query"])
+        if binfo["subc"]=="passre":
+            requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json=a)
+        else:
+            requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=a)
+        return
