904a4c4173417be5bf617c8752a5723e98ac9b0e
Shashaank Singh <tapash2004@outlook.com>
2021-05-25 04:30:17 +0530

    Update bquote.py

--- imports/buttons/bquote.py
+++ imports/buttons/bquote.py
@@ -21,8 +21,7 @@ def btn(aid:str,iid:str,token:str,binfo,usid:str):
             requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"components":[]}})
             c=requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json={"content":"<a:typing:597589448607399949>  Searching for Quotes..."})
             a=qfinder.qres(query=binfo["query"],userid=binfo["userid"])
-            d=requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/{c.json()['id']}",headers={"Content-Type": "application/json"},json=a)
-            print(d.json())
+            requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/{c.json()['id']}",headers={"Content-Type": "application/json"},json=a)
         else:
             requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"content":"<a:typing:597589448607399949>  Searching again...","embeds":[],"components":[]}})
             a=qfinder.qres(query=binfo["query"],userid=binfo["userid"])
