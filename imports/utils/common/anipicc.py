631c3853b16eec4e02eb6630fbfba9a682358e37
Shashaank Singh <tapash2004@outlook.com>
2021-05-26 05:21:19 +0530

    Update anipicc.py

--- imports/utils/common/anipicc.py
+++ imports/utils/common/anipicc.py
@@ -62,12 +62,14 @@ def pic(anim:bool,animal:str,usid:str):
         cont=":cat: Here's a cat pic for you." if anim==False else ":cat: Here's an animated cat pic for you."
         greet="Meow..."
         ftext="Powered by The Cat API"
+        ficon="https://cdn.discordapp.com/attachments/789798190353743874/794474344410906654/thecatapi_256xW.png"
     elif animal=="Dog":
         imgtype="png,jpg" if anim==False else "gif"
         url=requests.get(f"https://api.thedogapi.com/v1/images/search?size=small&mime_types={imgtype}",headers={"x-api-key":os.environ['DOG_API']}).json()[0]['url']
         cont=":cat: Here's a dog pic for you." if anim==False else ":dog: Here's an animated dog pic for you."
         greet="Woof..."
         ftext="Powered by The Dog API"
+        ficon="https://cdn.discordapp.com/attachments/789798190353743874/794491188643102730/Z.png"
     return {
         "flags": 64,
         "content": cont,
