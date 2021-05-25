71459d8b30a17242a25d9e6a4952b32fd2b4cb30
Shashaank Singh <tapash2004@outlook.com>
2021-05-26 05:13:35 +0530

    Update anipic.py

--- imports/commands/anipic.py
+++ imports/commands/anipic.py
@@ -2,4 +2,7 @@ import os,requests
 from imports.utils.common import anipicc
 
 def cmd(anim:bool,animal:str,usid:str):
-    return anipicc.pic(anim=anim,animal=animal,usid=usid)
+    return {
+        "type": 4,
+        "data": anipicc.pic(anim=anim,animal=animal,usid=usid)
+    }
