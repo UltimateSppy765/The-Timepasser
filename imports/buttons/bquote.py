35bffedff8be22d890a8b4b695c190d58649c27b
Shashaank Singh <tapash2004@outlook.com>
2021-05-27 11:22:22 +0530

    Update bquote.py

--- imports/buttons/bquote.py
+++ imports/buttons/bquote.py
@@ -18,9 +18,10 @@ def btn(aid:str,iid:str,token:str,binfo,usid:str):
         return
     else:        
         if binfo["subc"]=="passre":
+            query=binfo["query"]
             requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"components":[]}})
-            res=requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json={"embeds":[{"title":f":mag: Searching for Query: '{binfo["query"]}'","color":3092791}]}).json()
-            a=qfinder.qres(query=binfo["query"],userid=binfo["userid"])
+            res=requests.post(f"{baseurl}webhooks/{aid}/{token}",headers={"Content-Type": "application/json"},json={"embeds":[{"title":f":mag: Searching for Query: '{query}'","color":3092791}]}).json()
+            a=qfinder.qres(query=query,userid=binfo["userid"])
             requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/{res["id"]}",json=a)
         else:
             requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"embeds":[{"color":3092791,"title":":mag: Searching Again..."}],"components":[]}})
