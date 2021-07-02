dfc241a2a9b8f4ef7b6e9e4674f3eec23d7be3fd
UltimateSppy765 <tapash2004@outlook.com>
2021-07-03 01:51:01 +0530

    Add support for selects

--- imports/interactions/tthree.py
+++ imports/interactions/tthree.py
@@ -5,17 +5,20 @@ from imports.utils import fail
 def tthree(r):
     try:
         try:
-            b=json.loads(r.json["data"]["custom_id"])
+            c=json.loads(r.json["data"]["custom_id"])
         except:
             return fail.cfail()
-        bname=b["bfn"]
-        if bname=="dicereroll":
-            return dicereroll.btn(binfo=b,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
-        elif bname=="quote":
-            return bquote.btn(binfo=b,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
-        elif bname=="banipic":
-            return banipic.btn(binfo=b,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
-        else:
+        if r.json["data"]["component_type"]==2:
+            bname=c["bfn"]
+            if bname=="dicereroll":
+                return dicereroll.btn(binfo=c,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+            elif bname=="quote":
+                return bquote.btn(binfo=c,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+            elif bname=="banipic":
+                return banipic.btn(binfo=c,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+            else:
+                return fail.cfail()
+        elif r.json["data"]["component_type"]==3:
             return fail.cfail()
     except:
         return fail.err(traceback.format_exc())
