f740f7442d3e888e81ec01c4cb6961fdbd54b42e
UltimateSppy765 <tapash2004@outlook.com>
2021-08-11 21:06:36 +0530

    Prevent breaking changes

--- code.py
+++ code.py
@@ -1,20 +1,26 @@
 import os
+from imports.utils.fail import existnt
 from flask import Flask,request,jsonify,abort
-from discord_interactions import verify_key_decorator,InteractionType,InteractionResponseType
+from discord_interactions import verify_key_decorator
 from imports.interactions import ttwo,tthree
 
-app = Flask(__name__)
+app=Flask(__name__)
 
-@app.route('/',methods=['POST'])
-@verify_key_decorator(os.environ['CLIENT_ID'])
+#"it" refers to the interaction received unless specified.
+
+@app.route('/',methods=['POST']) #Don't mess with this, this is the default method we respond with.
+@verify_key_decorator(os.environ['CLIENT_ID']) #Don't mess with this, idk what it does.
 def code():
-    if request.json["type"]==2:
-        return jsonify(ttwo.slashc(r=request))
-    elif request.json["type"]==3:
+    if request.json["type"]==2: #Checks if it is an APPLICATION_COMMAND.
+        if request.json["data"]["type"]==1: #Checks and runs if it is a CHAT_INPUT command.
+            return jsonify(ttwo.slash_cmd.slashc(r=request))
+        else: #Runs this if it is a USER/MESSAGE command.
+            return existnt(request.json["data"]["name"])
+    elif request.json["type"]==3: #Checks and runs if it is a MESSAGE_COMPONENT.
         return jsonify(tthree.tthree(r=request))
 
-port=os.getenv('PORT')
-if port:
+port=os.getenv('PORT') #Gets the OS PORT that is open (stored in environment variables).
+if port: #Checks and runs the app if a PORT exists in the environment variables.
     app.run(host='0.0.0.0',port=port)
 else:
     print("Unable to find PORT environment variable.")
