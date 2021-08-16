3bea3eee12fca281c5f2b8597475e1d3f53d9597
UltimateSppy765 <tapash2004@outlook.com>
2021-08-16 20:59:08 +0530

    Little cleaning up of file

--- imports/interactions/tthree.py
+++ imports/interactions/tthree.py
@@ -2,23 +2,35 @@ import traceback,json
 from imports.buttons import *
 from imports.utils import fail
 
+buttons=["dicereroll","quote","banipic"]
+selects=[]
+
 def tthree(r):
     try:
-        try:
-            c=json.loads(r.json["data"]["custom_id"])
-        except:
-            return fail.cfail()
+        c=json.loads(r.json["data"]["custom_id"])
+    except:
+        return fail.cfail()
+    try:
         if r.json["data"]["component_type"]==2:
-            bname=c["bfn"]
-            if bname=="dicereroll":
-                return dicereroll.btn(binfo=c,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
-            elif bname=="quote":
-                return bquote.btn(binfo=c,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
-            elif bname=="banipic":
-                return banipic.btn(binfo=c,usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
-            else:
-                return fail.cfail()
+            return buttonitr(r=r,c=c)
         elif r.json["data"]["component_type"]==3:
-            return fail.cfail()
+            return selectitr(r=r,c=c)
     except:
         return fail.err(traceback.format_exc())
+
+def buttonitr(r,c):
+    bname=c["bfn"]
+    itruser=r.json["member"]["user"]
+    if not bname in buttons:
+        return fail.cfail()
+    if bname=="dicereroll":
+        return dicereroll.btn(binfo=c,usid=itruser["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+    elif bname=="quote":
+        return bquote.btn(binfo=c,usid=itruser["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+    elif bname=="banipic":
+        return banipic.btn(binfo=c,usid=itruser["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+
+def selectitr(r,c):
+    sname=c["sfn"]
+    if not sname in selects:
+        return fail.cfail()
