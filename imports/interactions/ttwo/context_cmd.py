9c42db378ec5208a423ca02c2265a7b577b87be7
UltimateSppy765 <tapash2004@outlook.com>
2021-08-16 10:10:33 +0530

    Some changes

--- imports/interactions/ttwo/context_cmd.py
+++ imports/interactions/ttwo/context_cmd.py
@@ -1,1 +1,23 @@
+import traceback
+from imports.utils import fail
+
+usctxcmds=["Avatar"]
+msgctxcmds=[]
+
 def contextc(r):
+    if r.json["data"]["type"]==2:
+        return uscontext(r)
+    elif r.json["data"]["type"]==3:
+        return msgcontext(r)
+    else:
+        return {"type":4,"data":{"flags":64,"content":"<:tickNo:315009174163685377> Context Command Type could not be identified.\n_Perhaps a new type in the API that I didn't hear of just yet..._"}}}
+
+def msgcontext(r):
+    cname=r.json["data"]["name"]
+    if not cname in msgctxcmds:
+        return fail.existnt(cname)
+
+def uscontext(r):
+    cname=r.json["data"]["name"]
+    if not cname in usctxcmds:
+        return fail.existnt(cname)
