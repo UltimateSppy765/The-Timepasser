b0a076fc5ba81020e5d2e849374b36cb756c2bd7
UltimateSppy765 <tapash2004@outlook.com>
2021-08-16 20:47:36 +0530

    Simplified stuff

--- imports/interactions/ttwo/slash_cmd.py
+++ imports/interactions/ttwo/slash_cmd.py
@@ -6,35 +6,37 @@ availablecmds=["aboutme","anipic","avatar","dice","echo","eval","guessnum","quot
 
 def slashc(r):
     cmdname=r.json["data"]["name"]
+    itruser=r.json["member"]["user"]
+    options=r.json["data"]["options"]
     if not cmdname in availablecmds:
         return fail.existnt(cmdname)
     else:
         try:
             if cmdname=="eval":
-                return eval.cmd(uid=r.json["member"]["user"]["id"],token=r.json["token"],iid=r.json["id"],sc=r.json["data"]["options"][0]["name"],aid=r.json["application_id"],jsn=r.json["data"]["options"][0]["options"][0]["value"])
+                return eval.cmd(uid=itruser["id"],token=r.json["token"],iid=r.json["id"],sc=options[0]["name"],aid=r.json["application_id"],jsn=options[0]["options"][0]["value"])
             elif cmdname=="aboutme":
-                return aboutme.cmd(token=r.json["token"],iid=r.json["id"],aid=r.json["application_id"],subc=r.json["data"]["options"][0]["name"],uid=r.json["member"]["user"]["id"])
+                return aboutme.cmd(token=r.json["token"],iid=r.json["id"],aid=r.json["application_id"],subc=options[0]["name"],uid=itruser["id"])
             elif cmdname=="dice":
-                return dice.cmd(usid=r.json["member"]["user"]["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
+                return dice.cmd(usid=itruser["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
             elif cmdname=="simon":
-                return simon.cmd(r.json["data"]["options"][0]["value"])
+                return simon.cmd(options[0]["value"])
             elif cmdname=="echo":
-                return echo.cmd(text=r.json["data"]["options"][0]["value"],uname=r.json["member"]["user"]["username"],id=r.json["member"]["user"]["id"],disc=r.json["member"]["user"]["discriminator"],av=r.json["member"]["user"]["avatar"])
+                return echo.cmd(text=options[0]["value"],uname=itruser["username"],id=itruser["id"],disc=itruser["discriminator"],av=itruser["avatar"])
             elif cmdname=="guessnum":
-                return guessnum.cmd(guess=r.json["data"]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"])
+                return guessnum.cmd(guess=options[0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"])
             elif cmdname=="avatar":
                 try:
-                    use=r.json["data"]["options"][0]["value"]
+                    use=options[0]["value"]
                 except KeyError:
                     use=None
-                return avatar.cmd(us=use,uname=r.json["member"]["user"]["username"],id=r.json["member"]["user"]["id"],disc=r.json["member"]["user"]["discriminator"],av=r.json["member"]["user"]["avatar"])
+                return avatar.cmd(us=use,uname=itruser["username"],id=itruser["id"],disc=itruser["discriminator"],av=itruser["avatar"])
             elif cmdname=="quote":
-                return quote.cmd(subc=r.json["data"]["options"][0]["name"],query=r.json["data"]["options"][0]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"],usid=r.json["member"]["user"]["id"])
+                return quote.cmd(subc=options[0]["name"],query=options[0]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"],usid=itruser["id"])
             elif cmdname=="anipic":
                 try:
-                    ani=r.json["data"]["options"][1]["value"]
+                    ani=options[1]["value"]
                 except:
                     ani=False
-                return anipic.cmd(usid=r.json["member"]["user"]["id"],animal=r.json["data"]["options"][0]["value"],anim=ani)
+                return anipic.cmd(usid=itruser["id"],animal=options[0]["value"],anim=ani)
         except:
             return fail.err(traceback.format_exc())
