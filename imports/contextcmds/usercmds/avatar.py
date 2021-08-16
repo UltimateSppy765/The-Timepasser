3d5f505d4468b7491e472ac1e09365c6a7f07c81
UltimateSppy765 <tapash2004@outlook.com>
2021-08-16 21:30:48 +0530

    Create context command for Avatar

---
+++ imports/contextcmds/usercmds/avatar.py
@@ -0,0 +1,18 @@
+from imports.utils import avpic
+import os
+
+def uscmd(usid,av,uname:str,disc):
+    return {
+        "type": 4,
+        "data": {
+            "embeds": [
+                {
+                    "title": f":mirror: {uname}'s Avatar:",
+                    "color": 3092791,
+                    "image": {
+                        "url": f"{avpic.usav(id=usid,discid=disc,av=av)}?size=256"
+                    }
+                }
+            ]
+        }
+    }
