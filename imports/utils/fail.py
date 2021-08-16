733d4b7e4d35a50ab124c88061f2476c5b132867
UltimateSppy765 <tapash2004@outlook.com>
2021-08-16 21:39:46 +0530

    Update fail.py

--- imports/utils/fail.py
+++ imports/utils/fail.py
@@ -29,6 +29,15 @@ def existnt(cname:str):
         }
     }
 
+def ctxexistnt(cname:str):
+    return {
+        "type": 4,
+        "data": {
+            "flags": 64,
+            "content": f"> <:tickNo:315009174163685377> Sorry to say but the context command you tried to use (which is `{cname}`) is currently unavailable (and probably under development or modification :tools:).\n> Please try again later."
+        }
+    }
+
 def cfail():
     return {
         "type": 4,
