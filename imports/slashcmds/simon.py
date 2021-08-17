6e64c7e92ee6f99f2750d1f7718fd10dc7a765a6
UltimateSppy765 <tapash2004@outlook.com>
2021-08-18 03:17:55 +0530

    `/simon` accepts max 500 characters long stuff now

--- imports/slashcmds/simon.py
+++ imports/slashcmds/simon.py
@@ -1,6 +1,14 @@
 from imports.utils import perspective
 
 def cmd(text:str):
+    if len(text)>500:
+        return {
+            "type": 4,
+            "data": {
+                "flags": 64,
+                "content": f"<:tickNo:315009174163685377> Simon can't say too long stuff. It can only speak upto 500 characters long _speeches_.\nYour speech is a whopping `{len(text)}` characters long."
+            }
+        }
     um=perspective.analyse(cont=text)
     if um is not None:
         return um
