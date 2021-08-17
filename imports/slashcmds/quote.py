6148ca7cbfabe027de302e1ed974e40f8549e034
UltimateSppy765 <tapash2004@outlook.com>
2021-08-18 03:32:12 +0530

    Update quote.py

--- imports/slashcmds/quote.py
+++ imports/slashcmds/quote.py
@@ -8,6 +8,14 @@ def cmd(subc:str,query:str,token:str,aid:str,iid:str,usid:str):
     if subc=="get":
         return {"type":4,"data":qget.getquote(type=query,userid=usid)}
     else:
+        if len(query)>100:
+            return {
+                "type": 4,
+                "data": {
+                    "flags": 64,
+                    "content": f"<:tickNo:315009174163685377> Your query cannot be greater than 100 characters. (your query is `{len(query)}` long)\nThis is to prevent breaking of the command. I hope you're smart enough to understand..."
+                }
+            }
         a=perspective.analyse(cont=query)
         if a is not None:
             return a
