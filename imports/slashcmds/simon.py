52d105878d7f7e017d8e4c840a3aa8386869befc
UltimateSppy765 <tapash2004@outlook.com>
2021-08-18 10:41:31 +0530

    Correct English

--- imports/slashcmds/simon.py
+++ imports/slashcmds/simon.py
@@ -6,7 +6,7 @@ def cmd(text:str):
             "type": 4,
             "data": {
                 "flags": 64,
-                "content": f"<:tickNo:315009174163685377> Simon can't say too long stuff. It can only speak upto 500 characters long _speeches_.\nYour speech is a whopping `{len(text)}` characters long."
+                "content": f"<:tickNo:315009174163685377> Simon can't say too long stuff. It can only give out _speeches_ upto 500 characters long.\nYour _speech_ is a whopping `{len(text)}` characters long."
             }
         }
     um=perspective.analyse(cont=text)
