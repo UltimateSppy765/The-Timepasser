199c735dbd50e15c00c088096552b290769f2068
UltimateSppy765 <tapash2004@outlook.com>
2021-08-18 10:26:12 +0530

    Update qfinder.py

--- imports/utils/quotes/qfinder.py
+++ imports/utils/quotes/qfinder.py
@@ -5,68 +5,76 @@ def qres(query:str,userid:str):
     ftext="Quotes from Wikiquote"
     ficon="https://cdn.discordapp.com/attachments/789798190353743874/794948919594450944/QqJDyLtUbgAAAAASUVORK5CYII.png"
     thum="https://cdn.discordapp.com/attachments/789798190353743874/796948926590615572/oie_transparent_1.png"
-    try:
-        quot=qlogic.qfind(query=query)
-    except Exception as l:
-        ename=type(l).__name__
-        if ename=="NoAuthorFound":
-            jsn={
-                "embeds": [
-                    {
-                        "title": f"No result found for query: **'{query}'**",
-                        "description": "Sorry, your query didn't work. Please try with a different one.",
-                        "color": 3092791,
-                        "thumbnail": {
-                            "url": thum
-                        },
-                        "footer": {
-                            "text": ftext,
-                            "icon_url": ficon
-                        }
-                    }
-                ]
-            }
-        elif ename=="NoQuoteFound":
-            jsn={
-                "embeds": [
-                    {
-                        "title": f"No result for query: **'{query}'**",
-                        "description": "Sorry, I couldn't find a quote for the given query. Please try again.",
-                        "color": 3092791,
-                        "thumbnail": {
-                            "url": thum
-                        },
-                        "footer": {
-                            "text": ftext,
-                            "icon_url": ficon
-                        }
-                    }
-                ],
-                "components": [{
-                    "type": 1,
-                    "components": [{
-                        "type": 2,
-                        "style": 2,
-                        "emoji": {
-                            "name": "reload",
-                            "id": "847699422389600256"
-                        },
-                        "label": "Try Again",
-                        "custom_id": json.dumps({"bfn":"quote","subc":"failre","userid":userid,"query":query},ensure_ascii=False).encode().decode()
-                    }]
-                }]
-            }
-        else:
-            return misc.err(traceback.format_exc())
-    else:
-        qt=quot[0]
-        autor=quot[1]         
+    Search=qlogic.qfind(query=query)
+    if Search[0]=="NoAuthorFound":
         jsn={
-            "content": "",
+            "embeds": [
+                {
+                    "title": f"No result found for query: **'{query}'**",
+                    "description": "Sorry, your query didn't work. Please try with a different one.",
+                    "color": 3092791,
+                    "thumbnail": {
+                        "url": thum
+                    },
+                    "footer": {
+                        "text": ftext,
+                        "icon_url": ficon
+                    }
+                }
+            ]
+        }
+    elif Search[0]=="NoQuoteFound":
+        Titles=Search[1].shuffle()
+        Suggestions=[]
+        for i in Titles:
+            if len(Suggestions)<26:
+                Suggestions.append({"label":i,"value":i,"emoji":{"name":"qauthor","id":"847687409034330132"}})
+            else:
+                break
+        jsn={
+            "embeds": [
+                {
+                    "title": f"No result for query: **'{query}'**",
+                    "description": "Sorry, I couldn't find a quote for the given query. Please try again.",
+                    "color": 3092791,
+                    "thumbnail": {
+                        "url": thum
+                    },
+                    "footer": {
+                        "text": ftext,
+                        "icon_url": ficon
+                    }
+                }
+            ],
+            "components": [{
+                "type": 1,
+                "components": [{
+                    "type": 3,
+                    "custom_id": json.dumps({"sfn":"quote","subc":"sgtns","userid":userid}),
+                    "placeholder": "🔎 Try searching",
+                    "options": Suggestions
+                }]
+              },
+              {
+                "type": 1,
+                "components": [{
+                    "type": 2,
+                    "style": 2,
+                    "emoji": {
+                        "name": "reload",
+                        "id": "847699422389600256"
+                    },
+                    "label": "Try Again",
+                    "custom_id": json.dumps({"bfn":"quote","subc":"failre","userid":userid,"query":query},ensure_ascii=False).encode().decode()
+                }]
+            }]
+        }
+    elif Search[0]=="Success":      
+        jsn={
             "embeds": [
                 {
                     "title": f"Search result for query: **'{query}'**",
-                    "description": f"{qt}\n- {autor}",
+                    "description": f"{Search[1]}\n- {Search[2]}",
                     "color": 3092791,
                     "thumbnail": {
                         "url": thum
@@ -87,7 +95,7 @@ def qres(query:str,userid:str):
                         "name": "quote",
                         "id": "847687355481718794"
                     },
-                    "custom_id": json.dumps({"bfn":"quote","subc":"passre","userid":userid,"query":query},ensure_ascii=False).encode().decode()
+                    "custom_id": json.dumps({"bfn":"quote","subc":"passre","userid":userid})
                 }]
             }]
         }
