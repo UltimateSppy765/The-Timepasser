6f38da0bb3a670f39bd7bfd2778b443c4e89e57a
UltimateSppy765 <tapash2004@outlook.com>
2021-08-19 21:55:48 +0530

    `/aboutme` updated with new assets

--- imports/slashcmds/aboutme.py
+++ imports/slashcmds/aboutme.py
@@ -6,5 +6,7 @@ baseurl=os.environ['BASE_URL']
 def cmd(token:str,iid:str,aid:str,subc:str,uid:str):
     if subc=="devs":
         requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":5,"data":{"flags":64}})
-    requests.post(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=meinfo(query=subc,uid=uid))
+        requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=meinfo(query=subc,uid=uid))
+    else:
+        requests.post(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=meinfo(query=subc,uid=uid))
     return
