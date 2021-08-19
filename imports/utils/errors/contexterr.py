a2197ed34932535fbe43e827e68719e6a3d62d1a
UltimateSppy765 <tapash2004@outlook.com>
2021-08-19 10:09:51 +0530

    Create contexterr.py

---
+++ imports/utils/errors/contexterr.py
@@ -0,0 +1,31 @@
+def contexterr(r,traceback):
+    if r.json["data"]["type"]==2:
+        ctxtype="USER"
+    elif r.json["data"]["type"]==3:
+        ctxtype="MESSAGE"
+    cmdname=r.json["data"]["name"]
+    itrid=r.json["id"]
+    a=int(itrid) >> 22
+    Tstamp=a+1420070400000
+    Content=f"Exception raised in context command: `{cmdname}`" 
+    Embed=[{
+        "title": "🔴 Traceback Called:",
+        "color": 15745587
+        "description": f"```py\n{traceback}\n```",
+        "fields": [{
+            "name": "Interaction Created at:",
+            "value": f"<t:{Tstamp}:F>",
+            "inline": True
+          },
+          {
+            "name": "Status:",
+            "value": "Not Fixed",
+            "inline": True
+          },
+          {
+            "name": "Context Command Type:",
+            "value": f"`{ctxtype}`",
+            "inline": True
+        }]
+    }]
+    return [Content,Embed]
