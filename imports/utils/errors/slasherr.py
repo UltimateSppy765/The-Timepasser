e17f97fbb0176683da4833b251f78faa948d2b36
UltimateSppy765 <tapash2004@outlook.com>
2021-08-19 09:40:28 +0530

    Slasherr?

--- imports/utils/errors/slasherr.py
+++ imports/utils/errors/slasherr.py
@@ -1,3 +1,22 @@
 def slashcerr(r,traceback):
     cmdname=r.json["data"]["name"]
-    return
+    itrid=r.json["id"]
+    a=int(itrid) >> 22
+    Tstamp=a+1420070400000
+    Content=f"Exception raised in slash command: `/{cmdname}`" 
+    Embed=[{
+        "title": "🔴 Traceback Called:",
+        "color": 15745587
+        "description": f"```py\n{traceback}\n```",
+        "fields": [{
+            "name": "Interaction Created at:",
+            "value": f"<t:{Tstamp}:F>",
+            "inline": True
+          },
+          {
+            "name": "Status:",
+            "value": "Not Fixed",
+            "inline": True
+        }]
+    }]
+    return [Content,Embed]
