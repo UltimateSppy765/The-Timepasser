8063e7f02f9c2557c48a8b94704ad3b1f0a70134
UltimateSppy765 <tapash2004@outlook.com>
2021-08-19 22:31:04 +0530

    I'm mad

--- imports/slashcmds/aboutme.py
+++ imports/slashcmds/aboutme.py
@@ -7,6 +7,6 @@ def cmd(token:str,iid:str,aid:str,subc:str,uid:str):
     if subc=="devs":
         requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":5,"data":{"flags":64}})
         requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=meinfo(query=subc,uid=uid))
+        return
     else:
-        requests.post(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=meinfo(query=subc,uid=uid))
-    return
+        return {"type":4,"data":meinfo(query=subc,uid=uid)}
