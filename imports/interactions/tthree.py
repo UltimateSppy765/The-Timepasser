a833de9ebcf1e3f9abdea182dda8f7590d6dd419
UltimateSppy765 <tapash2004@outlook.com>
2021-08-19 23:24:41 +0530

    Forgot to enable the select

--- imports/interactions/tthree.py
+++ imports/interactions/tthree.py
@@ -5,7 +5,7 @@ from imports.utils import fail
 from imports.utils.errors.errhandle import handle
 
 buttons=["dicereroll","quote","banipic"]
-selects=["quote"]
+selects=["quote","aboutme"]
 
 def tthree(r):
     try:
