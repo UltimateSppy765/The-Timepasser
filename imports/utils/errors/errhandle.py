c7d379f9dd5579fdd0c512c0736f9867c3de3912
UltimateSppy765 <tapash2004@outlook.com>
2021-08-24 01:26:05 +0530

    Error Response

--- imports/utils/errors/errhandle.py
+++ imports/utils/errors/errhandle.py
@@ -1,6 +1,8 @@
 import os,json,traceback,requests
 from imports.utils.errors import slasherr,contexterr,componenterr
+from random import choice
 
+tings=["Drank too much juice...","Lazed around too much...","**Started studying**...","Looked at myself in the mirror...","Walked into the 'I give up Pit'...","**Someone asked me whether I was smart...**"]
 baseurl=os.environ['BASE_URL']
 wid=os.environ['ERROR_WEBHOOK_ID']
 wtoken=os.environ['ERROR_WEBHOOK_TOKEN']
@@ -27,4 +29,19 @@ def handle(r,t):
         }]
     }
     requests.post(f"{baseurl}webhooks/{wid}/{wtoken}?wait=true",json=jsn)
+    jsn2={
+        "flags":64,
+        "content":f"<:tickNo:315009174163685377> Snap. I ran into an issue while attempting to do your bidding.\n**Reason:** {choice(tings)}\nYour interaction ID is `{r.json['id']}`. Please use this ID as reference if you wish to report this bug to our bot devs who can be found in their community server linked in the button below.",
+        "components": [{
+            "type": 1,
+            "components": [{
+                "type": 2,
+                "style": 5,
+                "label": "Our Community Server",
+                "url": "https://discord.gg/JXGe9MfXPF"
+            }]
+        }]
+    }
+    rq2=requests.post(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}",json=jsn2)
+    print(rq2.json())
     return
