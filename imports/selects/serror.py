d90970e80f1b4e269d3d003496471fc4e96644cd
UltimateSppy765 <tapash2004@outlook.com>
2021-08-24 00:30:52 +0530

    Working on Fix action implemented

--- imports/selects/serror.py
+++ imports/selects/serror.py
@@ -60,3 +60,19 @@ def select(msg,action:str,aid:str,iid:str,token:str,uid:str):
             jsn={"content":f"<:cross:879423049571663892> Failed to update error with Interaction ID: `{itrid}` ```\n{b.json()}\n```"}
         requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=jsn)
         return
+    elif action=="fixing":
+        requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":5,"data":{"flags":64}})
+        a=msg["embeds"][0]
+        a["color"]=16121600
+        a["fields"][1]["value"]=f"<:inworks:878990445286391809> Working on Fix\nMarked by <@!{uid}> <t:{tstamp}:R>"
+        z=msg["components"]
+        z[0]["components"][0]["options"][1]={"label":"Re-mark as Not Fixed","value":"notfixed","description":"Re-marks the error as not fixed.","emoji":{"name":"cross","id":"879423049571663892"}}
+        b=requests.patch(f"{baseurl}webhooks/{wid}/{wtoken}/messages/{msg['id']}",json={"embeds":[a],"components":z})
+        pattern=r'\**Interaction ID:\**\ (.*)\n'
+        itrid=re.search(pattern,msg["content"]).group(1)
+        if b.status_code==200:
+            jsn={"content":f"<:tick:847861518195884063> Successfully marked error with Interaction ID `{itrid}` as being worked on."}
+        else:
+            jsn={"content":f"<:cross:879423049571663892> Failed to update error with Interaction ID: `{itrid}` ```\n{b.json()}\n```"}
+        requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=jsn)
+        return
