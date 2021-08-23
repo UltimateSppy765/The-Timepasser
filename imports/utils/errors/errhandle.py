87b1e196ef0b8a872b4eef6c82ca2c70f51c3f98
UltimateSppy765 <tapash2004@outlook.com>
2021-08-24 04:48:34 +0530

    New error handler

--- imports/utils/errors/errhandle.py
+++ imports/utils/errors/errhandle.py
@@ -7,6 +7,24 @@ baseurl=os.environ['BASE_URL']
 wid=os.environ['ERROR_WEBHOOK_ID']
 wtoken=os.environ['ERROR_WEBHOOK_TOKEN']
 
+def usres(r,jsnres):
+    r1=requests.get(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}/messages/@original")
+    try:
+        msgflags=r1["flags"]
+    except:
+        return requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":4,"data":jsnres})
+    perms= 1 << 6 | 1 << 7
+    if (msgflags & perms) == perms:
+        requests.patch(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}/messages/@original",headers={"Content-Type":"application/json"},json=jsnres)
+        return
+    elif perms & 1 << 7:
+        requests.delete(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}/messages/@original")
+        requests.post(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}",headers={"Content-Type":"application/json"},json=jsnres)
+        return
+    else:
+        requests.post(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}",headers={"Content-Type":"application/json"},json=jsnres)
+        return
+
 def handle(r,t):
     if r.json["type"]==3:
         errlog=componenterr.componenterr(r=r,traceback=t)
@@ -31,7 +49,7 @@ def handle(r,t):
     requests.post(f"{baseurl}webhooks/{wid}/{wtoken}?wait=true",json=jsn)
     jsn2={
         "flags":64,
-        "content":f"<:tickNo:315009174163685377> Snap. I ran into an issue while attempting to do your bidding.\n**Reason:** {choice(tings)}\nYour interaction ID is `{r.json['id']}`. Please use this ID as reference if you wish to report this bug to our bot devs who can be found in their community server linked in the button below.",
+        "content":f"<:tickNo:315009174163685377> Snap. I ran into an issue while attempting to do your bidding.\n**Reason:** _{choice(tings)}_\nYour interaction ID is `{r.json['id']}`. Please use this ID as reference if you wish to report this bug to our bot devs who can be found in their community server linked in the button below.",
         "components": [{
             "type": 1,
             "components": [{
@@ -42,7 +60,4 @@ def handle(r,t):
             }]
         }]
     }
-    print(requests.get(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}/messages/@original").json())
-    rq2=requests.post(f"{baseurl}webhooks/{r.json['application_id']}/{r.json['token']}",headers={"Content-Type":"application/json"},json=jsn2)
-    print(rq2.json())
-    return
+    return usres(r=r,jsnres=jsn2)
