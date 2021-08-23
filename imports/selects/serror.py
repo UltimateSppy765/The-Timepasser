2c9ec375bf84aa97b9516fb594933eeaf0147db3
UltimateSppy765 <tapash2004@outlook.com>
2021-08-23 22:16:40 +0530

    _Let's see how this goes..._

--- imports/selects/serror.py
+++ imports/selects/serror.py
@@ -7,6 +7,9 @@ wtoken=os.environ["ERROR_WEBHOOK_TOKEN"]
 wid=os.environ["ERROR_WEBHOOK_ID"]
 
 def select(msg,action:str,aid:str,iid:str,token:str,uid:str):
+    t1=int(iid) >> 22
+    t2=t1+1420070400000
+    tstamp=t2//1000
     if uid not in ownerids():
         return {
             "type": 4,
@@ -21,9 +24,19 @@ def select(msg,action:str,aid:str,iid:str,token:str,uid:str):
         pattern=r'\**Interaction ID:\**\ (.*)\n'
         itrid=re.search(pattern,msg["content"]).group(1)
         if a.status_code==204:
-            jsn={"content":f"<:tick:847861518195884063> Successfully deleted error with Interaction ID: {itrid}"}
+            jsn={"content":f"<:tick:847861518195884063> Successfully deleted error with Interaction ID: `{itrid}`"}
         else:
-            jsn={"content":f"<:tickNo:315009174163685377> Failed to delete error with Interaction ID: {itrid} ```\n{a.json()}\n```"}
+            jsn={"content":f"<:tickNo:315009174163685377> Failed to delete error with Interaction ID: `{itrid}` ```\n{a.json()}\n```"}
         requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=jsn)
     elif action=="markfixed":
-        return fail.cfail()
+        requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":5,"data":{"flags":64}})
+        a=msg["embeds"][0]
+        a["color"]=64768
+        a["fields"][1]["value"]=f"<:tick:847861518195884063> Fixed\nMarked by <@!{uid}> <t:{tstamp}:R>"
+        requests.patch(f"{baseurl}webhooks/{wid}/{wtoken}/messages/{msg['id']}",json={"embeds":[a]})
+        if a.status_code==204:
+            jsn={"content":f"<:tick:847861518195884063> Successfully marked error with Interaction ID `{itrid}` as fixed."}
+        else:
+            jsn={"content":f"<:tickNo:315009174163685377> Failed to update error with Interaction ID: `{itrid}` ```\n{a.json()}\n```"}
+        requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=jsn)
+        return
