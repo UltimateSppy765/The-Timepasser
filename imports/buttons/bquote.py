8dd23e1d35e3b1cdfebc03f05766ee4e93b7f14e
UltimateSppy765 <tapash2004@outlook.com>
2021-08-25 03:49:38 +0530

    Test Regex on `Search Again!`

--- imports/buttons/bquote.py
+++ imports/buttons/bquote.py
@@ -16,13 +16,15 @@ def btn(aid:str,iid:str,token:str,binfo,msg,usid:str):
         requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":qget.getquote(type="random",userid=binfo["userid"])})
         return
     else:        
-        if binfo["subc"]=="qre":
+        if binfo["subc"]=="are":
             query=binfo["query"]
             requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"components":[],"embeds":[{"title":f":mag: Searching for Query: '{query}'","color":3092791}]}})
             a=qfinder.qres(query=query,userid=binfo["userid"])
             requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=a)
-        elif binfo["subc"]=="are":
+        elif binfo["subc"]=="passre":
+            pattern=r"query: \**\'(.*)'\**"
+            qry=re.search(pattern,msg["embeds"][0]["title"]).group(1)
             requests.post(f"{baseurl}interactions/{iid}/{token}/callback",json={"type":7,"data":{"embeds":[{"color":3092791,"title":":mag: Searching Again..."}],"components":[]}})
-            a=qfinder.qres(query=binfo["query"],userid=binfo["userid"])
+            a=qfinder.qres(query=qry,userid=binfo["userid"])
             requests.patch(f"{baseurl}webhooks/{aid}/{token}/messages/@original",json=a)
         return
