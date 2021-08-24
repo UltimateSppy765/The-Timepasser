473e930e688b00dd3c3cf82b03b0e60251d30e54
UltimateSppy765 <tapash2004@outlook.com>
2021-08-25 05:10:12 +0530

    Fixed `/avatar` instantly failing with no options

--- imports/interactions/ttwo/slash_cmd.py
+++ imports/interactions/ttwo/slash_cmd.py
@@ -8,32 +8,34 @@ availablecmds=["aboutme","anipic","avatar","dice","echo","eval","guessnum","quot
 def slashc(r):
     cmdname=r.json["data"]["name"]
     itruser=r.json["member"]["user"]
-    options=r.json["data"]["options"]
     if not cmdname in availablecmds:
         return fail.existnt(cmdname)
     else:
         try:
             if cmdname=="eval":
+                options=r.json["data"]["options"]
                 return eval.cmd(uid=itruser["id"],token=r.json["token"],iid=r.json["id"],sc=options[0]["name"],aid=r.json["application_id"],jsn=options[0]["options"][0]["value"])
             elif cmdname=="aboutme":
-                return aboutme.cmd(token=r.json["token"],iid=r.json["id"],aid=r.json["application_id"],subc=options[0]["name"],uid=itruser["id"])
+                return aboutme.cmd(token=r.json["token"],iid=r.json["id"],aid=r.json["application_id"],subc=r.json["data"]["options"][0]["name"],uid=itruser["id"])
             elif cmdname=="dice":
                 return dice.cmd(usid=itruser["id"],aid=r.json["application_id"],token=r.json["token"],iid=r.json["id"])
             elif cmdname=="simon":
-                return simon.cmd(options[0]["value"])
+                return simon.cmd(r.json["data"]["options"][0]["value"])
             elif cmdname=="echo":
-                return echo.cmd(text=options[0]["value"],uname=itruser["username"],id=itruser["id"],disc=itruser["discriminator"],av=itruser["avatar"])
+                return echo.cmd(text=r.json["data"]["options"][0]["value"],uname=itruser["username"],id=itruser["id"],disc=itruser["discriminator"],av=itruser["avatar"])
             elif cmdname=="guessnum":
-                return guessnum.cmd(guess=options[0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"])
+                return guessnum.cmd(guess=r.json["data"]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"])
             elif cmdname=="avatar":
                 try:
-                    use=options[0]["value"]
+                    use=r.json["data"]["options"][0]["value"]
                 except KeyError:
                     use=None
                 return avatar.cmd(us=use,uname=itruser["username"],id=itruser["id"],disc=itruser["discriminator"],av=itruser["avatar"])
             elif cmdname=="quote":
+                options=r.json["data"]["options"]
                 return quote.cmd(subc=options[0]["name"],query=options[0]["options"][0]["value"],aid=r.json["application_id"],iid=r.json["id"],token=r.json["token"],usid=itruser["id"])
             elif cmdname=="anipic":
+                options=r.json["data"]["options"]
                 ani=False
                 for i in options:
                     if i["name"]=="animal":
