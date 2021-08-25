37dba154ace2adb0a8038757a508f2f03e20b1a0
UltimateSppy765 <tapash2004@outlook.com>
2021-08-25 21:09:49 +0530

    Bruh#2

--- imports/buttons/shhguess.py
+++ imports/buttons/shhguess.py
@@ -5,7 +5,7 @@ def btn(usid:str):
         "type": 4,
         "data": {
             "flags": 64,
-            "content": f"Hey <@!{usid}>, you've found the hidden and **legendary** challenge of `Guess the Number`!\nTry your luck and see if you can win this challenge!\n__On winning you'll get a prize.__ :trophy:",
+            "content": f"Hey <@!{usid}>, you've found the hidden and **legendary** challenge of `Guess the Number`!\nTry your luck and see if you can win this challenge!\n_On winning you'll get a prize._ :trophy:",
             "components": []
         }
     }
