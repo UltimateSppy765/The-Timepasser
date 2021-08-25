adcce766a664e5196dfbd95b0efee52f127839f7
UltimateSppy765 <tapash2004@outlook.com>
2021-08-25 21:06:09 +0530

    Update shhguess.py

--- imports/buttons/shhguess.py
+++ imports/buttons/shhguess.py
@@ -2,7 +2,10 @@ import random
 
 def btn(usid:str):
     return {
-        "type": 7,
+        "type": 4,
         "data": {
+            "flags": 64,
+            "content": f"Hey <@!{usid}>, you've found the hidden and **legendary** challenge of `Guess the Number`!\nTry your luck and see if you can win this challenge!\n_On winning you'll get a prize.__ :trophy:",
+            "components": []
         }
     }
