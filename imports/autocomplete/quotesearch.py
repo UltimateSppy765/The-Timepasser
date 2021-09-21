1c0d689f849b1acf2c2082391d9357c07b298d61
UltimateSppy765 <tapash2004@outlook.com>
2021-09-22 03:00:38 +0530

    Update quotesearch.py

--- imports/autocomplete/quotesearch.py
+++ imports/autocomplete/quotesearch.py
@@ -1,1 +1,21 @@
-#Hello World.
+from imports.utils.quotes.qlogic import findtitles
+from random import sample
+
+def atc(input):
+    input=input.strip()
+    titles=findtitles(input)
+    if titles==[]:
+        return
+    if len(titles)>20:
+        list=sample(set(titles),20)
+    else:
+        list=titles
+    rtlist=[]
+    for i in list:
+        rtlist.append({"name":i,"value":i})
+    return {
+        "type": 8,
+        "data": {
+            "choices": rtlist
+        }
+    }
