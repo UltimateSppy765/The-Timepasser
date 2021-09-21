86517b8fa5a7e339cbf8bb446966b8a3bc7ba207
UltimateSppy765 <tapash2004@outlook.com>
2021-09-22 03:04:56 +0530

    Update tfour.py

--- imports/interactions/tfour.py
+++ imports/interactions/tfour.py
@@ -1,4 +1,6 @@
+from interactions.autocomplete import *
+
 def tfour(r):
     cmdname=r.json["data"]["name"]
     if cmdname=="quote":
-        return quotesearch.atc()
+        return quotesearch.atc(input=r.json["data"]["options"][0]["options"][0]["value"])
