d72c5190829f2d70b13b106237be6df6a4ca6cdf
UltimateSppy765 <tapash2004@outlook.com>
2021-09-22 03:18:36 +0530

    Error handling for type 4 interactions

--- imports/interactions/tfour.py
+++ imports/interactions/tfour.py
@@ -1,6 +1,11 @@
+import traceback
 from interactions.autocomplete import *
+from imports.utils.errors.errhandle import handle
 
 def tfour(r):
     cmdname=r.json["data"]["name"]
-    if cmdname=="quote":
-        return quotesearch.atc(input=r.json["data"]["options"][0]["options"][0]["value"])
+    try:
+        if cmdname=="quote":
+            return quotesearch.atc(input=r.json["data"]["options"][0]["options"][0]["value"])
+    except:
+        return handle(r=r,t=traceback.format_exc())
