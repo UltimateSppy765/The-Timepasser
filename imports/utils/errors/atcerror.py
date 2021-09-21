e934ebed50c67ab4e8c7fbcfe4f896caa2bfd2be
UltimateSppy765 <tapash2004@outlook.com>
2021-09-22 03:12:49 +0530

    Create atcerror.py

---
+++ imports/utils/errors/atcerror.py
@@ -0,0 +1,22 @@
+def atcerr(r,traceback):
+    cmdname=r.json["data"]["name"]
+    itrid=r.json["id"]
+    a=int(itrid) >> 22
+    Tstamp=a+1420070400000
+    Content=f"Exception raised in autocomplete for slash command: `/{cmdname}`" 
+    Embed=[{
+        "title": "🔴 Traceback Called:",
+        "color": 15745587,
+        "description": f"```py\n{traceback}\n```",
+        "fields": [{
+            "name": "Interaction Created at:",
+            "value": f"<t:{Tstamp//1000}:F>",
+            "inline": True
+          },
+          {
+            "name": "Status:",
+            "value": "<:cross:879423049571663892> Not Fixed",
+            "inline": True
+        }]
+    }]
+    return [Content,Embed]
