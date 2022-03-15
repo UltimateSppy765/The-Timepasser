10c655ca76ad9193635c5750b0f047526f6c32ff
UltimateSppy765 <tapash2004@outlook.com>
2022-03-16 02:22:50 +0530

    Delete imports directory

--- imports/slashcmds/anipic.py
+++
@@ -1,7 +0,0 @@
-from imports.utils.common import anipicc
-
-def cmd(anim:bool,animal:str,usid:str):
-    return {
-        "type": 4,
-        "data": anipicc.pic(anim=anim,animal=animal,usid=usid)
-    }
--- imports/utils/avpic.py
+++
@@ -1,7 +0,0 @@
-def usav(id:str,discid:str,av):
-    if av is None:
-        return f"https://cdn.discordapp.com/embed/avatars/{int(discid)%5}.png"
-    elif av.startswith("a_"):
-        return f"https://cdn.discordapp.com/avatars/{id}/{av}.gif"
-    else:
-        return f"https://cdn.discordapp.com/avatars/{id}/{av}.webp"
